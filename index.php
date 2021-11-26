8348aa5d43a645475c0013aa4e091241c5ddce72
Natan <natancom2n@gmail.com>
2021-11-26 16:16:46 -0300

    layout created

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="assets/css/style.css">
+    <title>Portifolio PHP</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Portifolio PHP</h1>
+        <h2>Indice Portifolio</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+        <nav class="modulos">
+            <div class="modulo verde">
+                <h3>Módulo 01</h3>
+                <ul>
+                    <li><a href="#">Exercício A</a></li>
+                    <li><a href="#">Exercício B</a></li>
+                    <li><a href="#">Exercício C</a></li>
+                </ul>
+            </div>
+            <div class="modulo vermelho">
+                <h3>Módulo 02</h3>
+                <ul>
+                    <li><a href="#">Exercício A</a></li>
+                    <li><a href="#">Exercício B</a></li>
+                    <li><a href="#">Exercício C</a></li>
+                </ul>
+            </div>
+            <div class="modulo azul">
+                <h3>Módulo 03</h3>
+                <ul>
+                    <li><a href="#">Exercício A</a></li>
+                    <li><a href="#">Exercício B</a></li>
+                    <li><a href="#">Exercício C</a></li>
+                </ul>
+            </div>
+            <div class="modulo roxo">
+                <h3>Módulo 04</h3>
+                <ul>
+                    <li><a href="#">Exercício A</a></li>
+                    <li><a href="#">Exercício B</a></li>
+                    <li><a href="#">Exercício C</a></li>
+                </ul>
+            </div>
+            <div class="modulo laranja">
+                <h3>Módulo 05</h3>
+                <ul>
+                    <li><a href="#">Exercício A</a></li>
+                    <li><a href="#">Exercício B</a></li>
+                    <li><a href="#">Exercício C</a></li>
+                </ul>
+            </div>
+            <div class="modulo verdeescuro">
+                <h3>Módulo 06</h3>
+                <ul>
+                    <li><a href="#">Exercício A</a></li>
+                    <li><a href="#">Exercício B</a></li>
+                    <li><a href="#">Exercício C</a></li>
+                </ul>
+            </div>
+            <div class="modulo vermescuro">
+                <h3>Módulo 07</h3>
+                <ul>
+                    <li><a href="#">Exercício A</a></li>
+                    <li><a href="#">Exercício B</a></li>
+                    <li><a href="#">Exercício C</a></li>
+                </ul>
+            </div>
+            <div class="modulo azulescuro">
+                <h3>Módulo 08</h3>
+                <ul>
+                    <li><a href="#">Exercício A</a></li>
+                    <li><a href="#">Exercício B</a></li>
+                    <li><a href="#">Exercício C</a></li>
+                </ul>
+            </div>
+            <div class="modulo roxoescuro">
+                <h3>Módulo 09</h3>
+                <ul>
+                    <li><a href="#">Exercício A</a></li>
+                    <li><a href="#">Exercício B</a></li>
+                    <li><a href="#">Exercício C</a></li>
+                </ul>
+            </div>
+        </nav>
+        </div>
+    </main>
+
+    <footer class="rodape">
+        NATAN ALVES -- 2019
+    </footer>
+</body> 
+</html>
